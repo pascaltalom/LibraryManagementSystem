b4c8dd7bc3cb2506c9d09269d727356c840aeb2c
pascaltalom <pascal.turbo9@gmail.com>
2024-06-15 13:27:53 +0200

    First Commit-Backend Project created

---
+++ librarymanager_backend/src/main/java/com/research/librarymanager_backend/LibrarymanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.research.librarymanager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagerBackendApplication.class, args);
+	}
+
+}
---
+++ librarymanager_backend/src/test/java/com/research/librarymanager_backend/LibrarymanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.librarymanager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
